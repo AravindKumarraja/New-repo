d658dd1231944415289f054b49a24fca36e80ae6
Aravindkumarraja <aravindkumarraja10@gmail.com>
2022-07-26 03:41:44 -0400

    Code

--- src/org/java/NewJava.java
+++ src/org/java/NewJava.java
@@ -3,7 +3,7 @@ package org.java;
 public class NewJava {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("Aravind");
+		System.out.println("Kumar");
 	}
 
 		
